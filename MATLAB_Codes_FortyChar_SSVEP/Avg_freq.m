b8d34d05136fa4b1b37189e0154d99dfb702a63b
Mohamed H. Zaky <mohamed.hossam.zaky@ieee.org>
2024-08-29 00:39:32 +0300

    Add files via upload

---
+++ MATLAB_Codes_FortyChar_SSVEP/Avg_freq.m
@@ -0,0 +1,109 @@
+clear
+clc
+
+% Specify the folder containing MAT files
+folderPath = 'C:\Users\mhz84\OneDrive\Desktop\Data analysis\Dataset_40C\';
+cd(folderPath)
+
+Sessfolders = dir(fullfile(folderPath));
+Sessfolders(1:2) = [];
+Flicker_time = 5;
+Samp_Freq = 250;
+Data_length = Flicker_time*Samp_Freq;
+freq_range = (Samp_Freq/Data_length)*(0:Data_length-1);
+NQ_rate_freq = freq_range(1:floor(Data_length/2)+1); %Nyquist rate
+
+% specify channels (1 - Fz,2 - C3,3 - Cz,4 - C4,5 - Pz,6 - PO7,7 - Oz,8 - PO8)
+Channels = {'Fz', 'C3', 'Cz', 'C4', 'Pz', 'PO7', 'Oz', 'PO8'};
+Charac = {'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9', 'N0',...
+    'LetterQ', 'LetterW', 'LetterE', 'LetterR', 'LetterT', 'LetterY',...
+    'LetterU', 'LetterI', 'LetterO', 'LetterP', 'LetterA', 'LetterS',...
+    'LetterD', 'LetterF', 'LetterG', 'LetterH', 'LetterJ', 'LetterK',...
+    'LetterL', 'LetterZ', 'LetterX', 'LetterC', 'LetterV', 'LetterB',...
+    'LetterN', 'LetterM', 'CtrlYES', 'CtrlNO', 'SPACE', 'TXT2VOICE'};
+Charac = Charac';
+Charac_num = length(Charac);
+Chan_num = length(Channels);
+Expected_Freq_Peak = [14, 14.2, 14.4, 14.6, 14.8, 15, 15.2, 15.4, 15.6, 13.8,...
+        11.8, 13, 9.4, 12, 12.4, 13.4, 12.6, 10.2, 11.4, 11.6,...
+        8.6, 12.2, 9.2, 9.6, 9.8, 10, 10.4, 10.6, 10.8,...
+        13.6, 13.2, 9, 12.8, 8.8, 11.2, 11,...
+        15.8, 8.4, 8, 8.2];
+Expected_Freq_Peak = Expected_Freq_Peak';
+
+% Calculate the number of runs in directory
+Total_num_runs = 0;
+% Iterate through each entry in the directory
+for i = 1:length(Sessfolders)
+    subfolder = fullfile(Sessfolders(i).folder,Sessfolders(i).name,'bdf');
+    cd(subfolder)
+    Files = dir(fullfile(subfolder, '*.mat'));
+    files_num = length(Files);
+    Total_num_runs = Total_num_runs + files_num;
+end
+Avg_runs = zeros(Chan_num,Data_length,Total_num_runs,Charac_num);
+
+% Get a list of all folders
+counter = 0;
+for f = 1:length(Sessfolders)
+    subfolder = fullfile(Sessfolders(f).folder,Sessfolders(f).name,'bdf');
+    cd(subfolder)
+    Files = dir(fullfile(subfolder, '*.mat'));
+    files_num = length(Files);
+    for r = 1:files_num
+        R = load(fullfile(Files(r).folder,Files(r).name));
+        eeg_4D = R.Data.EEG;
+        mean_block = mean(eeg_4D,3);
+        Avg_runs(:,:,r+counter,:) = mean_block;
+    end
+    counter = counter + files_num;
+end
+
+% Create folders to save images per channel
+parentFolder = 'C:\Users\mhz84\OneDrive\Desktop\Data analysis\Avg_freq_Dataset_40C';
+for i = 1:Chan_num
+    subfolderName = sprintf('Chan%d', i);
+    fullSubfolderPath = fullfile(parentFolder, subfolderName);
+    mkdir(fullSubfolderPath);
+end
+
+% Plot grand average epochs per channel
+GrandAvg = squeeze(mean(Avg_runs,3));
+% Band-pass filter design (adjust cut-off frequencies as needed)
+lowCut = 11; % Hz
+highCut = 21; % Hz
+[b, a] = butter(5, [lowCut highCut]/(Samp_Freq/2)); % Design 5th order Butterworth filter
+
+for Chan2plot = 1:Chan_num            
+    subfolderName = sprintf('Chan%d', Chan2plot);
+    chan_data = squeeze(GrandAvg(Chan2plot,:,:));
+    MaxP4freq = zeros(1,Charac_num);
+    FhzMax = zeros(1,Charac_num);
+    for n = 1:Charac_num
+        epoch = chan_data(:,n);
+        filtered_data = filtfilt(b, a, epoch);
+        figure,plot(NQ_rate_freq',filtered_data(1:length(NQ_rate_freq),1),'k','linew',2)
+        set(gca, 'xlim', [12 20])
+        xlabel('Frequency (Hz)')
+        ylabel('Power/Frequency (V2/Hz)')
+        title(['Grand average power spectra from channel ' Channels(Chan2plot) ' and character ' Charac(n)])
+        grid
+        baseFileName = ['Channel_',num2str(Chan2plot),'_GrandAvg_Epoch_',num2str(n),'.png'];
+        fullFileName = fullfile(parentFolder, subfolderName, baseFileName);
+        saveas(gcf, fullFileName);
+        close all
+        ROI = 41:81; % Freq 12 to 20 Hz
+        filtered_data_ROI = filtered_data(ROI,1);
+        [MaxP, Id] = max(filtered_data_ROI);
+        MaxP4freq(1,n) = MaxP;
+        NQ_rate_freq_ROI = NQ_rate_freq(1,ROI);
+        FreqHz = NQ_rate_freq_ROI(Id);
+        FhzMax(1,n) = FreqHz;
+    end
+    cd(fullfile(parentFolder, subfolderName))
+    MaxP4freq = MaxP4freq';
+    FhzMax = FhzMax';
+    QA_table = table(Charac,MaxP4freq,FhzMax,Expected_Freq_Peak);
+    writetable(QA_table,'QA.txt','Delimiter', '\t');
+    type 'QA.txt';
+end
---
+++ MATLAB_Codes_FortyChar_SSVEP/PreprocessEEG.m
@@ -0,0 +1,149 @@
+clear
+clc
+eeglab
+
+% Specify the folder containing BDF files
+folderPath = 'C:\Users\mhz84\OneDrive\Desktop\Data analysis\Dataset_40C\';  % Replace with your actual folder pah where diffferent sets are
+cd(folderPath)
+Session_dir = dir(folderPath);
+Session_dir(1:2) = [];
+
+for s = 1:length(Session_dir)
+    % Get a list of all BDF files
+    bdf_dir = fullfile(Session_dir(s).folder, Session_dir(s).name, 'bdf');
+    cd(bdf_dir)
+    files = dir(fullfile(bdf_dir, '*.bdf'));
+    
+    for f = 1:length(files)
+        % Get the filename
+        filename = files(f).name;
+        % Split filename
+        baseName = erase(string(filename), '.bdf'); 
+        baseName = erase(string(baseName), 'UnicornRecorder_');
+        
+        % Read the BDF data using csvread
+        EEG = pop_biosig(filename);
+        
+        % Fix the names and locations of channels in a static way since they will
+        % be the same every time
+        
+        chanlocs = struct('labels', {'Fz', 'C3', 'Cz', 'C4', 'Pz', 'PO7', 'Oz', 'PO8'});
+        EEG.chanlocs = chanlocs;
+        EEG = pop_chanedit(EEG,'lookup', 'D:\Research\Graduation Project\eeglab2024.0\plugins\dipfit\standard_BEM\elec\standard_1005.elc');
+        for chan = 1:length(EEG.chanlocs)
+            EEG.chanlocs(chan).type = 'EEG';
+        end
+        
+        % Re-reference to average
+        EEG = pop_reref(EEG, []);
+        % Filtering and re-referencing were done in the previous code
+        % PrepareMAT
+        % Apply ASR
+        %     EEG_ASR = pop_clean_rawdata(EEG, 'clean_method', 'asr');
+        % Identify removed channels
+        %     removed_channels = setdiff({EEG.chanlocs.labels}, {EEG_ASR.chanlocs.labels});
+        % Interpolate missing channels
+        %     EEG_interpo = pop_interp(EEG_ASR, EEG.chanlocs);        
+        % Run ICA
+        %     EEG_ICA = pop_runica(EEG_interpo, 'icatype', 'runica');
+        EEG_ICA = pop_runica(EEG, 'icatype', 'runica');
+        % Label bad ICs automatically
+        EEG_label = pop_iclabel(EEG_ICA, 'Default');
+        % Access IC labels
+        classes = EEG_label.etc.ic_classification.ICLabel.classes;
+        % Define artifact criteria (adjust based on your needs)
+        artifact_classes = {'Muscle', 'Eye', 'Line Noise'};
+        % Identify artifact components
+        bad_components = find(ismember(classes, artifact_classes));
+        % Remove artifact components
+        EEG_label_removed = pop_subcomp(EEG_label, bad_components); 
+        % Filter to only the frequencies of interest
+        EEG_filt = pop_eegfiltnew(EEG_label_removed, 'locutoff', 7, 'hicutoff', 17); 
+        % Compare two plots
+%         eegplot(EEG.data, 'data2', EEG_filt.data, 'srate', EEG.srate); 
+        
+        % Get all unique event codes
+        unique_events = unique({EEG_filt.event.type});
+        % Define epoch window
+        epoch_window = [0 5]; % flicker duration is 5 secondes
+        % Extract epochs and create a new dataset with baseline correction
+        EEG_EPOCH_preprocess = pop_epoch(EEG_filt, unique_events, epoch_window, 'newname', 'my_epochs_all', 'epochinfo', 'yes', 'baseline', [-0.5 0]);
+        
+        % Fix the names of trials
+        Charac = {'N1', 'N2', 'N3', 'N4', 'N5', 'N6', 'N7', 'N8', 'N9', 'N0',...
+        'LetterQ', 'LetterW', 'LetterE', 'LetterR', 'LetterT', 'LetterY',...
+        'LetterU', 'LetterI', 'LetterO', 'LetterP', 'LetterA', 'LetterS',...
+        'LetterD', 'LetterF', 'LetterG', 'LetterH', 'LetterJ', 'LetterK',...
+        'LetterL', 'LetterZ', 'LetterX', 'LetterC', 'LetterV', 'LetterB',...
+        'LetterN', 'LetterM', 'CtrlYES', 'CtrlNO', 'SPACE', 'TXT2VOICE'};
+        
+        num_rounds = length(EEG_EPOCH_preprocess.urevent)/length(Charac);
+        counter = 0;
+        for round = 1:num_rounds
+            for i = counter+1:counter+length(Charac)
+                label = i-(round-1)*length(Charac);
+                new_label = Charac{1, label};
+                EEG_EPOCH_preprocess.urevent(i).type = new_label;
+            end
+            counter = counter + length(Charac);
+        end
+        
+        % Save the EEG dataset
+        % Define filepath and filename
+        filepath = bdf_dir;
+        EEG_name = char(strcat("Preprocessed_SSVEP_BDF2SET_Unicorn_", baseName));
+        % Save the current dataset
+        EEG_filt = pop_saveset(EEG_filt, 'filepath', filepath, 'filename', EEG_name);
+                
+        % Save the EPOCH dataset
+        % Define filepath and filename
+        filepath = bdf_dir;
+        EPOCHname = char(strcat("Preprocessed_EPOCH_SSVEP_BDF2SET_Unicorn_", baseName));
+        % Save the current dataset
+        EEG_EPOCH_preprocess = pop_saveset(EEG_EPOCH_preprocess, 'filepath', filepath, 'filename', EPOCHname);
+        
+        % Create a 4D matrix for AI (channels x data x blocks x characters)
+        Unorganized_data = EEG_EPOCH_preprocess.data;
+        flicker_length = 5; % in seconds
+        FourDim_matrix = zeros(length(chanlocs),flicker_length*EEG_EPOCH_preprocess.srate,num_rounds,length(Charac));
+        
+        for c = 1:length(chanlocs)
+            for j = 1:length(Charac)
+                counter = 0;
+                for i = 1:num_rounds
+                    FourDim_matrix(c, :, i, j) = squeeze(Unorganized_data(c, :, j+counter));
+                    counter = counter + length(Charac);
+                end
+            end
+        end
+        
+        Expected_Freq_Peak = [14, 14.2, 14.4, 14.6, 14.8, 15, 15.2, 15.4, 15.6, 13.8,...
+        11.8, 13, 9.4, 12, 12.4, 13.4, 12.6, 10.2, 11.4, 11.6,...
+        8.6, 12.2, 9.2, 9.6, 9.8, 10, 10.4, 10.6, 10.8,...
+        13.6, 13.2, 9, 12.8, 8.8, 11.2, 11,...
+        15.8, 8.4, 8, 8.2];
+        Phase = [180, 270, 0, 90, 180, 270, 0, 90, 180, 270,...
+        0, 90, 180, 270, 0, 90, 180, 270, 0, 90,...
+        180, 270, 0, 90, 180, 270, 0, 90, 180,...
+        270, 0, 90, 180, 270, 0, 90,...
+        180, 270, 0, 90];
+        
+        Data = [];
+        Data.EEG = FourDim_matrix;
+        Data.suppl_info.name = 'Name';
+        Data.suppl_info.age = 'Age';
+        Data.suppl_info.gender = 'Gender';
+        Data.suppl_info.date = 'Recording date';
+        Data.suppl_info.session = 'session number';
+        Data.suppl_info.run = f;
+        Data.suppl_info.time = 'day/night';
+        Data.suppl_info.chan = EEG_EPOCH_preprocess.chanlocs;
+        Data.suppl_info.freqs = Expected_Freq_Peak;
+        Data.suppl_info.phases = Phase;
+        Data.suppl_info.charac = Charac;
+        Data.suppl_info.srate = EEG_filt.srate;
+        Data.suppl_info.orientation = 'chan x data x block x charac';
+        AI_name = char(strcat('Preprocessed_', Session_dir(s).name, '_', baseName, '.mat'));
+        save(AI_name,'Data')
+    end
+end
